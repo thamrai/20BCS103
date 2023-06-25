bb0633cda1eec0853f01eac4c1bdecc3f69126b4
Naresh.T <100408283+thamrai@users.noreply.github.com>
2023-06-25 12:31:26 +0530

    trainschedule.js

---
+++ src/trainschedule.js
@@ -0,0 +1,18 @@
+const trainSchedule = [{
+    trainname: "chennai exp",
+    trainnumber: "2344",
+    departureTime: {
+        hours: 21,
+        minutes: 35,
+        seconds: 0
+    },
+    seatesAvailble: {
+        sleeper: 3,
+        AC: 1
+    },
+    price: {
+        sleeper: 2,
+        AC: 5
+    },
+    delayedBY: 15,
+}]
