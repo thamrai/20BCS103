7806cdb88aa624acd97620583728a509a5107e1d
Naresh.T <100408283+thamrai@users.noreply.github.com>
2023-06-25 12:28:44 +0530

    singletrain.js

---
+++ singletrain.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SingleTrain = () => {
+  return (
+    <div>
+      <h1>Single Train</h1>
+      {/* Display the details of a single train */}
+    </div>
+  );
+};
+
+export default SingleTrain;
